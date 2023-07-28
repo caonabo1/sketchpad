083a1bd38dc116eb7fb2051edfc0c9bf36ae395c
caonabo1 <130840190+caonabo1@users.noreply.github.com>
2023-07-28 20:30:35 +0300

    modify change of color

--- script.js
+++ script.js
@@ -1,8 +1,24 @@
+let gridColor;
+let colorLightness;
+let lightnessDecrease;
 const container = document.getElementById('container');
 const btn = document.querySelector('button');
+
 container.addEventListener('mouseover', (e) => {
     if (e.target != container) {
-        e.target.style.background = 'black'
+        if (!e.target.style.background) {
+            e.target.style.hsl = gridColor;
+            e.target.style.background = gridColor;            
+        } else {
+            const squareColor = e.target.style.hsl;
+            const squareHSLValues = squareColor.split(',');
+            const oldLightnessVal = Number(squareHSLValues[2].slice(1, -2));
+            const newLightnessVal = ` ${oldLightnessVal - lightnessDecrease}%)`;
+            squareHSLValues[2] = newLightnessVal;
+            const newColor = squareHSLValues.join(',');
+            e.target.style.hsl = newColor;
+            e.target.style.background = newColor;
+        }
     }
 });
 
@@ -15,7 +31,17 @@ btn.addEventListener('click', () => {
     }
 });
 
+function getColor() {
+    const h = Math.round(Math.random() * 360);
+    const s = Math.round(Math.random() * 100);
+    const l = Math.round(Math.random() * 100);
+    colorLightness = l;
+    return `hsl(${h}, ${s}%, ${l}%)`;
+}
+
 function createGrid(size) {
+    gridColor = getColor();
+    lightnessDecrease = colorLightness / 10;
     container.innerHTML = '';
     const gap = 2 * (size - 1);
     for (let i = 0; i < (size * size); i++) {
